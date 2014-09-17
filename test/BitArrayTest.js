f2bc879c0367c9c1427115b0b847f5e97ce653f1
achingbrain <alex@achingbrain.net>
2014-09-17 14:04:11 +0100

    fix up reading from bit arrays

--- test/BitArrayTest.js
+++ test/BitArrayTest.js
@@ -13,4 +13,40 @@ describe('BitArray', function() {
     expect(bitArray.getArray()[2]).to.equal(parseInt("00010000", 2))
     expect(bitArray.getArray()[3]).to.equal(parseInt("00110010", 2))
   })
+
+  it('should read first bit', function() {
+    var bitArray = new BitArray([
+      parseInt("10000000", 2)
+    ])
+
+    expect(bitArray.getBits(0, 0, 1)).to.equal(1)
+  })
+
+  it('should read bits accross byte boundaries', function() {
+    var bitArray = new BitArray([
+      parseInt("00000001", 2),
+      parseInt("10000000", 2)
+    ])
+
+    expect(bitArray.getBits(0, 7, 2)).to.equal(3)
+  })
+
+  it('should move the read pointer when called repeatedly', function() {
+    var bitArray = new BitArray([
+      parseInt("10100000", 2)
+    ])
+
+    expect(bitArray.getBits(0, 0, 1)).to.equal(1)
+    expect(bitArray.getBits(0, 0, 1)).to.equal(0)
+    expect(bitArray.getBits(0, 0, 1)).to.equal(1)
+  })
+
+  it('should read bits with byte offsets', function() {
+    var bitArray = new BitArray([
+      parseInt("00000000", 2),
+      parseInt("10000000", 2)
+    ])
+
+    expect(bitArray.getBits(1, 0, 1)).to.equal(1)
+  })
 })
