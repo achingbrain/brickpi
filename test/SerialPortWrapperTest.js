7b7fd6587f9816791ee06a4c2c356f76aa94354b
achingbrain <alex@achingbrain.net>
2014-09-25 11:00:38 +0100

    handle multiple messages

--- test/SerialPortWrapperTest.js
+++ test/SerialPortWrapperTest.js
@@ -2,15 +2,26 @@ var expect = require('chai').expect,
   sinon = require('sinon'),
   proxyquire = require('proxyquire')
 
+var checksum = function(data) {
+  return data.length + data.reduce(function(a, b) {
+    return a + b;
+  });
+}
+
 describe('SerialPortWrapper', function() {
 
-  var SerialPortWrapper, SerialPort, LED
+  var SerialPortWrapper, SerialPort, clock
 
-  before(function() {
+  beforeEach(function() {
+    clock = sinon.useFakeTimers()
+
     SerialPort = function() {
       this.write = sinon.stub()
       this.flush = sinon.stub()
       this.on = sinon.stub()
+
+      this.flush.callsArg(0)
+      this.write.callsArg(1)
     }
 
     SerialPortWrapper = proxyquire('../lib/SerialPortWrapper', {
@@ -20,14 +31,16 @@ describe('SerialPortWrapper', function() {
     })
   })
 
+  afterEach(function() {
+    clock.restore()
+  })
+
   it('should send bytes to the serial port', function(done) {
     var chipIndex = 0
     var command = 0x01
     var data = [0x02, 0x03, 0x04]
 
     var serialPortWrapper = new SerialPortWrapper()
-    serialPortWrapper._serialPort.flush.callsArg(0)
-    serialPortWrapper._serialPort.write.callsArg(1)
 
     serialPortWrapper.write(chipIndex, command, data, function() {
       var buffer = serialPortWrapper._serialPort.write.getCall(0).args[0]
@@ -48,8 +61,6 @@ describe('SerialPortWrapper', function() {
     var data = [0x01, 0x02, 0x03]
 
     var serialPortWrapper = new SerialPortWrapper()
-    serialPortWrapper._serialPort.flush.callsArg(0)
-    serialPortWrapper._serialPort.write.callsArg(1)
 
     serialPortWrapper.on('_response_1', function(error, buffer) {
       expect(buffer[0]).to.equal(data[0])
@@ -59,19 +70,13 @@ describe('SerialPortWrapper', function() {
       done()
     })
 
-    var checksum = data.length + data.reduce(function(a, b) {
-      return a + b;
-    });
-
-    serialPortWrapper._read([checksum, data.length].concat(data))
+    serialPortWrapper._read([checksum(data), data.length].concat(data))
   })
 
   it('should read bytes from the serial port in batches', function(done) {
     var data = [0x01, 0x02, 0x03]
 
     var serialPortWrapper = new SerialPortWrapper()
-    serialPortWrapper._serialPort.flush.callsArg(0)
-    serialPortWrapper._serialPort.write.callsArg(1)
 
     serialPortWrapper.on('_response_1', function(error, buffer) {
       expect(buffer[0]).to.equal(data[0])
@@ -81,11 +86,7 @@ describe('SerialPortWrapper', function() {
       done()
     })
 
-    var checksum = data.length + data.reduce(function(a, b) {
-      return a + b;
-    });
-
-    serialPortWrapper._read([checksum, data.length])
+    serialPortWrapper._read([checksum(data), data.length].concat(data))
     serialPortWrapper._read(data)
   })
 
@@ -93,8 +94,6 @@ describe('SerialPortWrapper', function() {
     var data = [0x01, 0x02, 0x03]
 
     var serialPortWrapper = new SerialPortWrapper()
-    serialPortWrapper._serialPort.flush.callsArg(0)
-    serialPortWrapper._serialPort.write.callsArg(1)
 
     serialPortWrapper.on('_response_1', function(error, buffer) {
       expect(buffer[0]).to.equal(data[0])
@@ -104,11 +103,7 @@ describe('SerialPortWrapper', function() {
       done()
     })
 
-    var checksum = data.length + data.reduce(function(a, b) {
-      return a + b;
-    });
-
-    serialPortWrapper._read([checksum])
+    serialPortWrapper._read([checksum(data)])
     serialPortWrapper._read([data.length])
     serialPortWrapper._read([data[0]])
     serialPortWrapper._read([data[1]])
@@ -119,8 +114,6 @@ describe('SerialPortWrapper', function() {
     var data = [0x01, 0x02, 0x03]
 
     var serialPortWrapper = new SerialPortWrapper()
-    serialPortWrapper._serialPort.flush.callsArg(0)
-    serialPortWrapper._serialPort.write.callsArg(1)
 
     serialPortWrapper.on('_response_1', function(error, buffer) {
       expect(error instanceof Error).to.be.true
@@ -131,5 +124,44 @@ describe('SerialPortWrapper', function() {
     var checksum = 1
 
     serialPortWrapper._read([checksum, data.length].concat(data))
+  })
+
+  it('should time out if response does not arrive in time', function(done) {
+    var data = [0x01, 0x02, 0x03]
+
+    var serialPortWrapper = new SerialPortWrapper()
+
+    serialPortWrapper.on('_response_1', function(error, buffer) {
+      expect(error).to.be.ok
+      expect(error.message).to.contain('timeout')
+
+      done()
+    })
+
+    serialPortWrapper._read([checksum(data), data.length].concat([data[0]]))
+
+    clock.tick(serialPortWrapper._readTimeoutMs + 100)
+  })
+
+  it('should handle back to back messages', function(done) {
+    var data = [0x01, 0x02, 0x03]
+
+    var serialPortWrapper = new SerialPortWrapper()
+    var responseCount = 0
+
+    serialPortWrapper.on('_response_1', function(error, buffer) {
+      expect(error).to.undefined
+
+      responseCount++
+
+      if(responseCount == 2) {
+        done()
+      }
+    })
+
+    var packet = [checksum(data), data.length].concat(data)
+    packet = packet.concat(packet)
+
+    serialPortWrapper._read(packet)
   })
 })
